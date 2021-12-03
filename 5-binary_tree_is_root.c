c8e0f47216773e9b4f1de0ddf8b0cd794adc1937
AndresBlandonW <andblandon@hotmail.com>
2021-12-02 21:05:29 -0800

    update task binary root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_is_root - check if a node is a root
  * @node: pointer to the node to check
+ * Return: validation of node
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
